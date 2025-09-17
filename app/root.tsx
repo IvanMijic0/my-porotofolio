68b42fd52d1733ae337aa6614d87501e58579382
Ivan Mijic <ivan@epifront.com>
2025-09-17 05:53:46 +0200

    {@IvanMijic0}[main]{fix): Responsive Design.

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,8 @@ export const Layout = ({ children }: PropsWithChildren) => (
 	<html lang="en">
 		<head>
 			<meta charSet="utf-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="viewport"
+				content="width=device-width, initial-scale=1, viewport-fit=cover" />
 			<Meta />
 			<Links />
 		</head>
